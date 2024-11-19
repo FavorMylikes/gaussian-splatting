59f42b1c72f10b297042614ab8f1888f9ed20724
Matt Li <FavorMylikes@users.noreply.github.com>
2024-11-19 17:23:14 -0500

    use `lpips` package directly

--- metrics.py
+++ metrics.py
@@ -15,11 +15,11 @@ from PIL import Image
 import torch
 import torchvision.transforms.functional as tf
 from utils.loss_utils import ssim
-from lpipsPyTorch import lpips
 import json
 from tqdm import tqdm
 from utils.image_utils import psnr
 from argparse import ArgumentParser
+import lpips
 
 def readImages(renders_dir, gt_dir):
     renders = []
